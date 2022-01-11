b064aa6a0e2cc787c0b47d90829cb87aca3ad521
Arthur Simonyan <1simonyanartur1@gmail.com>
2022-01-11 17:50:10 +0300

    work on equipment section

---
+++ src/layout/blocks/equipment/equipment.js
@@ -0,0 +1,33 @@
+export function equipment() {
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: "auto",
+		spaceBetween: 30,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
+
+	// var swiper = new Swiper("..equipment .slider-wrap", {
+	// 	slidesPerView: 1,
+	// 	spaceBetween: 10,
+	// 	pagination: {
+	// 		el: ".swiper-pagination",
+	// 		clickable: true,
+	// 	},
+	// 	breakpoints: {
+	// 		640: {
+	// 			slidesPerView: 2,
+	// 			spaceBetween: 20,
+	// 		},
+	// 		768: {
+	// 			slidesPerView: 4,
+	// 			spaceBetween: 40,
+	// 		},
+	// 		1024: {
+	// 			slidesPerView: 5,
+	// 			spaceBetween: 50,
+	// 		},
+	// 	},
+	// });
+}
