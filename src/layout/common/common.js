ff8eaa976643349bf87dc98b516e95d79dea7f69
Arthur Simonyan <1simonyanartur1@gmail.com>
2022-01-14 12:25:58 +0300

    add prices page

--- src/layout/common/common.js
+++ src/layout/common/common.js
@@ -2,7 +2,6 @@ import { ba } from "../blocks/ba/ba";
 import { contact } from "../blocks/contact/contact";
 import { equipment } from "../blocks/equipment/equipment";
 import { faq } from "../blocks/faq/faq";
-import { foto } from "../blocks/foto/foto";
 import { hamburger } from "../blocks/hamburger/hamburger";
 import { header } from "../blocks/header/header";
 import { reviews } from "../blocks/reviews/reviews";
@@ -17,7 +16,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	reviews();
 	contact();
 	faq();
-	foto();
+
+	var tabLink = document.querySelectorAll('.tabs__link');
+	var tabTab = document.querySelectorAll('.tabs__tab');
+
+	tabTab.forEach(function (tabTabItem) {
+		tabTabItem.style.display = 'none';
+	});
+
+	tabTab[0].style.display = 'block';
+
+	tabLink.forEach(function (tabLinkItem) {
+		tabLinkItem.addEventListener('click', function (e) {
+			e.preventDefault();
+			let tabName = this.dataset.tab;
+			tabLink.forEach(function (tabLinkItem3) {
+				tabLinkItem3.classList.remove('active');
+			});
+			this.classList.add('active');
+
+			tabTab.forEach(function (tabTabItem) {
+				if (tabTabItem.getAttribute('data-tab') == tabName) {
+					tabTabItem.style.display = 'block';
+				} else {
+					tabTabItem.style.display = 'none';
+				}
+			});
+		});
+	});
 });
 
 window.addEventListener('resize', function (e) {
